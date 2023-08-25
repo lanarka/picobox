71e1d8d73195dc27ca3a87ea1343583f7e930730
palky <palee1701@gmail.com>
2023-08-25 19:46:45 +0200

    Replace pinouts.h

--- include/hardware.h
+++ include/hardware.h
@@ -1,18 +1,18 @@
 /*
-    PICOBOX
-    Pinouts definition for pico-sdk
-    Board Version: 1
+    PICOBOX - Board Version: 1.1
+    Hardware Board/Pinouts definition
 */
 
-#ifndef __PINOUTS_H__
-#define __PINOUTS_H__
+#ifndef __HARDWARE_H__
+#define __HARDWARE_H__
 
 // PORTS
-#define PORT_ETHSD spi1
-#define PORT_RADIO spi0
-#define PORT_UART1 uart0
-#define PORT_UART2 uart1
-#define PORT_I2C   i2c1
+#define PORT_ETHSD      spi1
+#define PORT_RADIO      spi0
+#define PORT_UART1      uart0
+#define PORT_UART2      uart1
+#define PORT_I2C        i2c1
+#define PORT_I2C_SLAVE  i2c0
 
 // PINS
 #define PIN_UART1_TX    0   // UART 1
@@ -21,22 +21,22 @@
 #define PIN_I2C_DAT     2   // MAIN I2C BUS
 #define PIN_I2C_CLK     3
 
-#define PIN_D1          4   // DIGITAL I/O
+#define PIN_D0          4   // DIGITAL I/O
 
 #define PIN_LED         5   // STATUS LED
 #define PIN_BUZZER      6   // BUZZER
 
 #define PIN_SD_CS       7   // SD CARD
 
-#define PIN_UART2_TX    8   // UART 2
+#define PIN_UART2_TX    8   // UART 2 OR SLAvE I2C BUS
 #define PIN_UART2_RX    9
 
-#define PIN_D2          10  // DIGITAL I/O
-#define PIN_D3          11
+#define PIN_D1          10  // DIGITAL I/O
+#define PIN_D2          11
 
 #define PIN_ETHSD_MISO  12  // ETHERNET+SDCARD
 
-#define PIN_D4          13  // DIGITAL I/O
+#define PIN_D3          13  // DIGITAL I/O
 
 #define PIN_ETHSD_SCK   14  // ETHERNET
 #define PIN_ETHSD_MOSI  15
@@ -51,8 +51,8 @@
 
 #define PIN_BUTTON      22  // FUNC BUTTON
 
-#define PIN_INA1        26  // I/O (Analogs)
-#define PIN_INA2        27
-#define PIN_INA3        28
+#define PIN_A0          26  // I/O (Analogs)
+#define PIN_A1          27
+#define PIN_A2          28
 
 #endif
